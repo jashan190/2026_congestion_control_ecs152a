ce065546216e603d0f975d4e8824e5369c9dd777
Jashan190 <singhjashandeep190@gmail.com>
2026-02-09 11:37:21 -0800

    initial commit

---
+++ sender_stop_and_wait_Jashan_921273042_Rafi_[student_id2].py
@@ -0,0 +1,9 @@
+
+size_packet = 1024
+
+
+#implement udp sender with stop and wait protocol 
+#size is 1024 bytes, correct number of packets is to be sent, sequence needs to be tracked
+#measure over 10 iterations 
+#metric = 𝑀𝑒𝑡𝑟𝑖𝑐 = 0. 3 𝑇ℎ𝑟𝑜𝑢𝑔ℎ𝑝𝑢𝑡/1000 + 0. 7/𝐴𝑣𝑒𝑟𝑎𝑔𝑒 𝐷𝑒𝑙𝑎𝑦 𝑝𝑒𝑟 𝑃𝑎𝑐𝑘𝑒𝑡
+
